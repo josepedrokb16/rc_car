7b0163c112a9e728856f341e102e2450d045067e
Jose Pedro Kitajima Borges <borges@joses-mbp.speedport.ip>
2024-08-26 21:17:54 +0200

    working with queue

--- src/XboxController/xbox_controller.py
+++ src/XboxController/xbox_controller.py
@@ -2,18 +2,20 @@ import pygame
 import asyncio
 
 class XboxController:
-    def __init__(self):
+    def __init__(self, input_queue):
         pygame.init()
         self.valid_axis = [0,4,5]
         self.remote_controller = pygame.joystick.Joystick(0)
         self.remote_controller.init()
+        self.input_queue = input_queue
 
-    async def get_event(self):
-        event = pygame.event.poll()
-        if event.type == pygame.JOYAXISMOTION and event.axis in self.valid_axis:
-            event_map = {
-                "axis": int(event.axis),
-                "value": float(event.value)
-            }
-            return event_map
-        return None
+    async def monitor_events(self):
+        while True:
+            await asyncio.sleep(1/60)
+            for event in pygame.event.get():
+                if event.type == pygame.JOYAXISMOTION and event.axis in self.valid_axis:
+                    event_map = {
+                        "axis": int(event.axis),
+                        "value": float(event.value)
+                    }
+                    await self.input_queue.put(event_map)
